16389b8f0dcf752696164d8f84b12d08c63c5053
imamy4 <imamy4@github.com>
2015-05-11 18:42:20 +0300

    Добавлен SprintsContoller, соответствующие вью и модельки

---
+++ УправлениеПроектами/Controllers/SpintsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using МенеджерБД.Домен;
+using УправлениеПроектами.Models.КлассыДляФормВвода;
+
+namespace УправлениеПроектами.Controllers
+{
+    public class SprintsController : BaseController
+    {
+        /// <summary>
+        /// Выводит актуальные спринты
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View(МенеджерБД.Записи<Спринт>().Where(проект => проект.ДатаНачала <= DateTime.Now && проект.ДатаКонца >= DateTime.Now));
+        }
+
+        /// <summary>
+        /// Основная страница конкретного спринта
+        /// </summary>
+        /// <param name="id">Id проекта</param>
+        /// <returns></returns>
+        public ActionResult Sprint(int id)
+        {
+            return View(МенеджерБД.ПолучитьЗаписьБДПоId<Спринт>(id));
+        }
+
+        /// <summary>
+        /// Страница создания нового спринта
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            СпринтДляФормы новыйСпринт = new СпринтДляФормы();
+            return View(новыйСпринт);
+        }
+
+        /// <summary>
+        /// Страница создания нового спринта, с проверкой на валидность введенных значений
+        /// </summary>
+        /// <param name="спринт"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Create(СпринтДляФормы спринт)
+        {
+            bool отменитьСохранение = false;
+
+            // проверка на наличие проекта
+            var проект = МенеджерБД.ПолучитьЗаписьБДПоId<Проект>(спринт.IdПроекта);
+            if (проект == null)
+            {
+                ModelState.AddModelError("IdПроекта", "Необходимо выбрать проект");
+                отменитьСохранение = true;
+            }
+
+            if (string.IsNullOrWhiteSpace(спринт.Название))
+            {
+                ModelState.AddModelError("Название", "Необходимо заполнить название");
+                отменитьСохранение = true;
+            }
+
+            if (спринт.ДатаНачала >= спринт.ДатаКонца)
+            {
+                ModelState.AddModelError("ДатаКонца", "Дата окончания не может быть меньше даты начала проекта");
+                отменитьСохранение = true;
+            }
+
+            // проверка на то, что границы спринта входят в границы проекта
+            if (проект != null && проект.ДатаНачала > спринт.ДатаНачала)
+            {
+                ModelState.AddModelError("ДатаНачала", "Спринт не может начаться раньше проекта");
+                отменитьСохранение = true;
+            }
+
+            if (проект != null && проект.ДатаКонца < спринт.ДатаКонца)
+            {
+                ModelState.AddModelError("ДатаКонца", "Спринт не может заканчиваться после окончания проекта");
+                отменитьСохранение = true;
+            }
+
+            if (!отменитьСохранение)
+            {
+                Спринт новыйСпринт = спринт.ПеревестиВСущностьБД();
+
+                МенеджерБД.СоздатьЗаписьБД<Спринт>(новыйСпринт);
+
+                return RedirectToAction("Success", new { id = новыйСпринт.Id });
+            }
+
+            return View(спринт);
+        }
+
+        /// <summary>
+        /// Страница с уведомлением об удачном создании спринта
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Success(int id)
+        {
+            return View(МенеджерБД.ПолучитьЗаписьБДПоId<Спринт>(id));
+        }
+
+    }
+}
---
+++ УправлениеПроектами/Models/КлассыДляФормВвода/СпринтДляФормы.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using МенеджерБД.Домен;
+
+namespace УправлениеПроектами.Models.КлассыДляФормВвода
+{
+    public class СпринтДляФормы : БазоваяМодельСущностиБД<Спринт>
+    {
+        public int IdПроекта { get; set; }
+
+        public IEnumerable<SelectListItem> ПроектыSelectList
+        {
+            get
+            {
+                foreach (Проект проект in МенеджерБД.АктуальныеПроекты())
+                {
+                    yield return new SelectListItem
+                    {
+                        Value = проект.Id.ToString(),
+                        Text = проект.Название.ToString(),
+                        Selected = false
+                    };
+                }
+            }
+        }
+
+        public string Название { get; set; }
+
+        public DateTime ДатаНачала
+        {
+            get
+            {
+                return new DateTime(ГодДатаНачала, МесяцДатаНачала, ДеньДатаНачала);
+            }
+        }
+
+        public int ДеньДатаНачала { get; set; }
+
+        public int МесяцДатаНачала { get; set; }
+
+        public int ГодДатаНачала { get; set; }
+
+        public IEnumerable<SelectListItem> ДеньДатаНачалаSelectList
+        {
+            get
+            {
+                for (int i = 1; i < 32; i++)
+                {
+                    yield return new SelectListItem
+                    {
+                        Value = i.ToString(),
+                        Text = i.ToString(),
+                        Selected = ДеньДатаНачала == i
+                    };
+                }
+            }
+        }
+
+        public IEnumerable<SelectListItem> МесяцДатаНачалаSelectList
+        {
+            get
+            {
+                for (int i = 1; i < 13; i++)
+                {
+                    yield return new SelectListItem
+                    {
+                        Value = i.ToString(),
+                        Text = new DateTime(2013, i, 1).ToString("MMMM"),
+                        Selected = МесяцДатаНачала == i
+                    };
+                }
+            }
+        }
+
+        public IEnumerable<SelectListItem> ГодДатаНачалаSelectList
+        {
+            get
+            {
+                for (int i = 2013; i <= DateTime.Now.Year + 2; i++)
+                {
+                    yield return new SelectListItem
+                    {
+                        Value = i.ToString(),
+                        Text = i.ToString(),
+                        Selected = ГодДатаНачала == i
+                    };
+                }
+            }
+        }
+
+        public DateTime ДатаКонца
+        {
+            get
+            {
+                return new DateTime(ГодДатаКонца, МесяцДатаКонца, ДеньДатаКонца, 23, 59, 59);
+            }
+        }
+
+        public int ДеньДатаКонца { get; set; }
+
+        public int МесяцДатаКонца { get; set; }
+
+        public int ГодДатаКонца { get; set; }
+
+        public IEnumerable<SelectListItem> ДеньДатаКонцаSelectList
+        {
+            get
+            {
+                for (int i = 1; i < 32; i++)
+                {
+                    yield return new SelectListItem
+                    {
+                        Value = i.ToString(),
+                        Text = i.ToString(),
+                        Selected = ДеньДатаКонца == i
+                    };
+                }
+            }
+        }
+
+        public IEnumerable<SelectListItem> МесяцДатаКонцаSelectList
+        {
+            get
+            {
+                for (int i = 1; i < 13; i++)
+                {
+                    yield return new SelectListItem
+                    {
+                        Value = i.ToString(),
+                        Text = new DateTime(2013, i, 1).ToString("MMMM"),
+                        Selected = МесяцДатаКонца == i
+                    };
+                }
+            }
+        }
+
+        public IEnumerable<SelectListItem> ГодДатаКонцаSelectList
+        {
+            get
+            {
+                for (int i = 2013; i <= DateTime.Now.Year + 2; i++)
+                {
+                    yield return new SelectListItem
+                    {
+                        Value = i.ToString(),
+                        Text = i.ToString(),
+                        Selected = ГодДатаКонца == i
+                    };
+                }
+            }
+        }
+    }
+}
