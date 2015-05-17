02d6097457636200fcd638a721ad67a22df5528a
imamy4 <imamy4@github.com>
2015-05-17 13:21:08 +0300

    (МенеджерБД) Убраны поля внешних Id

--- МенеджерБД/Домен/Спринт.cs
+++ МенеджерБД/Домен/Спринт.cs
@@ -14,7 +14,7 @@ namespace МенеджерБД.Домен
         }
 
         public virtual int Id { get; set; }
-        public virtual int IdПроекта { get; set; }
+
         public virtual string Название { get; set; }
         public virtual DateTime ДатаНачала { get; set; }
         public virtual DateTime ДатаКонца { get; set; }
